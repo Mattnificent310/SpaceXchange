10263a764a5e8dc58dab9086e2cc391365f6b539
Matt Maree <matt.maree@tracker.co.za>
2018-07-24 13:39:07 +0200

    Development
    API

--- src/main/java/com/example/rest/repositories/BuyerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.rest.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.rest.models.Buyer;
-
-public interface BuyerRepo extends CrudRepository<Buyer, Long>{
-
-}
